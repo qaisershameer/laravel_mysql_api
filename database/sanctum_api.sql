2a0c057c4491293de00d0100d40f0f5ab5517171
Qaiser Shameer <naeem.malik009@gmail.com>
2024-10-26 13:41:11 +0500

    26 Oct 2024

--- database/sanctum_api.sql
+++ database/sanctum_api.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 09, 2024 at 08:40 AM
+-- Generation Time: Oct 26, 2024 at 08:23 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -43,6 +43,24 @@ CREATE TABLE `accounts` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `accounts`
+--
+
+INSERT INTO `accounts` (`acId`, `acTitle`, `email`, `mobile`, `openingBal`, `CurrentBal`, `uId`, `currencyId`, `accTypeId`, `parentId`, `areaId`, `created_at`, `updated_at`) VALUES
+(2, 'Syed Burhan', 'syedburhan@gmail.com', '+923346013608', 0, 0, 1, 1, 2, 1, 2, '2024-10-14 03:33:12', '2024-10-17 03:53:30'),
+(3, 'Qaiser Shameer', 'qaisershameer@gmail.com', '+923346013608', 0, 0, 1, 3, 6, 1, 2, '2024-10-14 03:37:00', '2024-10-16 02:47:28'),
+(4, 'Farhad Ahmd', 'farhadahmd@gmail.com', '+923346013608', 0, 0, 1, 2, 2, 1, 3, '2024-10-14 03:40:05', '2024-10-17 03:52:22'),
+(5, 'Syed Mustafa', 'syedmustafa@gmail.com', '+923346013609', 0, 0, 1, 7, 2, 1, 1, '2024-10-16 01:30:15', '2024-10-16 01:30:15'),
+(6, 'Muhmmad Muzamil', 'muhmmadmuzamil@gmail.com', '+923346013608', 0, 0, 1, 2, 2, 1, 4, '2024-10-16 02:10:47', '2024-10-17 03:50:00'),
+(7, 'Mujahid Ameen', 'mujahidameen@gmail.com', '+923346013608', 0, 0, 1, 2, 1, 1, 2, '2024-10-16 02:17:56', '2024-10-17 03:01:22'),
+(12, 'Faizan Ayub', 'faizanayub@gmail.com', '+923346013608', 0, 0, 1, 1, 1, 1, 4, '2024-10-17 02:24:40', '2024-10-17 02:24:40'),
+(13, 'ABL', 'abl@gmail.com', '+923346013608', 0, 0, 1, 1, 3, 1, 4, '2024-10-17 03:42:41', '2024-10-17 03:42:41'),
+(15, 'HMB', 'hmb@gmail.com', '+923346013608', 0, 0, 1, 1, 3, 1, 4, '2024-10-17 03:43:11', '2024-10-17 03:43:11'),
+(16, 'UBL', 'ubl@gmail.com', '+923346013608', 0, 0, 1, 1, 3, 1, 4, '2024-10-17 03:43:23', '2024-10-17 03:43:23'),
+(17, 'HBL', 'hbl@gmail.com', '+923346013608', 0, 0, 1, 1, 3, 1, 4, '2024-10-17 03:49:49', '2024-10-17 03:49:49'),
+(18, 'Danish Kaneria', 'danishkaneria@gmail.com', '+923346013608', 0, 0, 1, 1, 1, 1, 1, '2024-10-17 03:53:03', '2024-10-17 03:53:03');
+
 -- --------------------------------------------------------
 
 --
@@ -58,6 +76,16 @@ CREATE TABLE `accparent` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `accparent`
+--
+
+INSERT INTO `accparent` (`parentId`, `accParentTitle`, `accTypeId`, `uId`, `created_at`, `updated_at`) VALUES
+(1, 'PARENT 001', 1, 1, '2024-10-12 02:08:14', '2024-10-12 02:08:14'),
+(2, 'PARENT 002', 2, 1, '2024-10-12 02:08:29', '2024-10-12 02:08:29'),
+(3, 'PARENT 003', 3, 1, '2024-10-12 02:08:40', '2024-10-12 02:08:40'),
+(4, 'PARENT 004', 4, 1, '2024-10-12 02:08:48', '2024-10-12 02:08:48');
+
 -- --------------------------------------------------------
 
 --
@@ -72,6 +100,20 @@ CREATE TABLE `acctype` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `acctype`
+--
+
+INSERT INTO `acctype` (`accTypeId`, `accTypeTitle`, `uId`, `created_at`, `updated_at`) VALUES
+(1, 'CUSTOMER', 1, '2024-10-12 01:51:34', '2024-10-12 01:51:34'),
+(2, 'SUPPLIER', 1, '2024-10-12 01:51:43', '2024-10-12 01:51:43'),
+(3, 'BANK', 1, '2024-10-12 01:51:50', '2024-10-12 01:51:50'),
+(4, 'ASSETS', 1, '2024-10-12 01:51:57', '2024-10-12 01:51:57'),
+(5, 'LIABILITY', 1, '2024-10-12 01:52:03', '2024-10-12 01:52:03'),
+(6, 'CAPITAL', 1, '2024-10-12 01:52:10', '2024-10-12 01:52:10'),
+(7, 'REVENUE', 1, '2024-10-12 01:52:17', '2024-10-12 01:52:17'),
+(8, 'EXPENSE', 1, '2024-10-12 01:52:24', '2024-10-12 01:52:24');
+
 -- --------------------------------------------------------
 
 --
@@ -85,6 +127,16 @@ CREATE TABLE `area` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `area`
+--
+
+INSERT INTO `area` (`areaId`, `areaTitle`, `uId`, `created_at`, `updated_at`) VALUES
+(1, 'MULTAN', 1, '2024-10-12 03:20:27', '2024-10-12 03:20:27'),
+(2, 'LAHORE', 1, '2024-10-12 03:21:55', '2024-10-12 03:22:03'),
+(3, 'KARACHI', 1, '2024-10-12 03:22:16', '2024-10-12 03:22:16'),
+(4, 'ISLMABAD', 1, '2024-10-12 03:22:26', '2024-10-12 03:22:26');
 
 -- --------------------------------------------------------
 
@@ -123,6 +175,16 @@ CREATE TABLE `currency` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `currency`
+--
+
+INSERT INTO `currency` (`currencyId`, `currencyTitle`, `uId`, `created_at`, `updated_at`) VALUES
+(1, 'PKR', 1, '2024-10-09 03:12:24', '2024-10-09 03:12:24'),
+(2, 'SAR', 1, '2024-10-09 03:17:22', '2024-10-09 03:17:22'),
+(3, 'USD', 1, '2024-10-09 03:18:04', '2024-10-10 01:15:01'),
+(7, 'YUAN', 1, '2024-10-10 03:24:00', '2024-10-10 03:24:00');
 
 -- --------------------------------------------------------
 
@@ -178,7 +240,7 @@ CREATE TABLE `personal_access_tokens` (
 
 INSERT INTO `personal_access_tokens` (`id`, `tokenable_type`, `tokenable_id`, `name`, `token`, `abilities`, `last_used_at`, `expires_at`, `created_at`, `updated_at`) VALUES
 (1, 'App\\Models\\User', 1, 'API Token', '81b370e48c0b6788b90d7ca7a6ba09a5550d164bbb0e56b0969c1d9460c16608', '[\"*\"]', NULL, NULL, '2024-10-09 01:01:17', '2024-10-09 01:01:17'),
-(2, 'App\\Models\\User', 1, 'API Token', '1e47c6b131d176e2286ab6d3a2544883d2cfddcd665523b3a001491506c3c8b8', '[\"*\"]', NULL, NULL, '2024-10-09 01:03:24', '2024-10-09 01:03:24');
+(2, 'App\\Models\\User', 1, 'API Token', '1e47c6b131d176e2286ab6d3a2544883d2cfddcd665523b3a001491506c3c8b8', '[\"*\"]', '2024-10-24 03:29:14', NULL, '2024-10-09 01:03:24', '2024-10-24 03:29:14');
 
 -- --------------------------------------------------------
 
@@ -237,6 +299,14 @@ CREATE TABLE `vouchers` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `vouchers`
+--
+
+INSERT INTO `vouchers` (`voucherId`, `voucherDate`, `voucherPrefix`, `remarksMaster`, `sumDebit`, `sumCredit`, `sumDebitSR`, `sumCreditSR`, `uId`, `created_at`, `updated_at`) VALUES
+(1, '2024-10-24', 'CR', 'Cash Received.', 0, 600, 0, 6, 1, '2024-10-24 03:05:22', '2024-10-24 03:05:22'),
+(2, '2024-10-24', 'CR', 'Cash Received.', 0, 1000, 0, 10, 1, '2024-10-24 03:10:07', '2024-10-24 03:10:07');
+
 -- --------------------------------------------------------
 
 --
@@ -256,6 +326,14 @@ CREATE TABLE `vouchersdetail` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `vouchersdetail`
+--
+
+INSERT INTO `vouchersdetail` (`voucherDtid`, `voucherId`, `uId`, `acId`, `remarksDetail`, `debit`, `credit`, `debitSR`, `creditSR`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, 13, 'Cash Received.', 0, 600, 0, 6, '2024-10-24 03:05:22', '2024-10-24 03:05:22'),
+(2, 2, 1, 17, 'Cash Received.', 0, 1000, 0, 10, '2024-10-24 03:10:07', '2024-10-24 03:10:07');
 
 --
 -- Indexes for dumped tables
@@ -364,31 +442,31 @@ ALTER TABLE `vouchersdetail`
 -- AUTO_INCREMENT for table `accounts`
 --
 ALTER TABLE `accounts`
-  MODIFY `acId` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `acId` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `accparent`
 --
 ALTER TABLE `accparent`
-  MODIFY `parentId` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `parentId` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `acctype`
 --
 ALTER TABLE `acctype`
-  MODIFY `accTypeId` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `accTypeId` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `area`
 --
 ALTER TABLE `area`
-  MODIFY `areaId` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `areaId` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `currency`
 --
 ALTER TABLE `currency`
-  MODIFY `currencyId` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `currencyId` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `migrations`
@@ -418,13 +496,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `vouchers`
 --
 ALTER TABLE `vouchers`
-  MODIFY `voucherId` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `voucherId` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `vouchersdetail`
 --
 ALTER TABLE `vouchersdetail`
-  MODIFY `voucherDtid` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `voucherDtid` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
